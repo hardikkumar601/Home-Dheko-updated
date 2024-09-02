d6367b16ecb02527f8106c5613f44548754c7ff8
Hardik Kumar <hardikkumar601@gmail.com>
2024-09-03 00:01:38 +0530

    port commit

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const Mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require("dotenv").config();
-const authRoutes = require('./routes/auth.js');
-const listingRoutes = require('./routes/listing.js')
-const userRoutes = require('./routes/user.js')
-const bookingRoutes = require('./routes/booking.js')
+const express = require("express");
+const Mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+const authRoutes = require("./routes/auth.js");
+const listingRoutes = require("./routes/listing.js");
+const userRoutes = require("./routes/user.js");
+const bookingRoutes = require("./routes/booking.js");
 
 const app = express();
 app.use(cors());
@@ -13,18 +13,17 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Routes
-app.use('/auth',authRoutes);
-app.use('/properties',listingRoutes);
-app.use('/user',userRoutes);
-app.use('/bookings',bookingRoutes);
+app.use("/auth", authRoutes);
+app.use("/properties", listingRoutes);
+app.use("/user", userRoutes);
+app.use("/bookings", bookingRoutes);
 
 // Mongoose connection
-const PORT = 3000;
-Mongoose
-       .connect(process.env.MONGO_URL,{
-        dbName: "Dream_Nest",
-      })
-      .then(() => {
-        app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-      })
-      .catch((err) => console.log(`${err} did not connect`));
+const port = process.env.PORT;
+Mongoose.connect(process.env.MONGO_URL, {
+  dbName: "Dream_Nest",
+})
+  .then(() => {
+    app.listen(port, () => console.log(`Server Port: ${port}`));
+  })
+  .catch((err) => console.log(`${err} did not connect`));
